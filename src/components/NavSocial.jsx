45c460cf6dc653c77f0796f3a982ae31695f52d6
matteolpera <matteo.lepera02@gmail.com>
2025-11-25 17:33:43 +0100

    feat: aggiunta la social bar

--- src/components/NavSocial.jsx
+++ src/components/NavSocial.jsx
@@ -1,13 +1,19 @@
+import Facebook from "../assets/img/footer-facebook.png"
+import Twitter from "../assets/img/footer-twitter.png"
+import Youtube from "../assets/img/footer-youtube.png"
+import Pinterest from "../assets/img/footer-pinterest.png"
+import Periscope from "../assets/img/footer-periscope.png"
+
 export default function NavSocial() {
     return (
         <nav>
-            <ul>
-                <h3>FOLLOW US</h3>
-                <li><img src="" alt="" /></li>
-                <li><img src="" alt="" /></li>
-                <li><img src="" alt="" /></li>
-                <li><img src="" alt="" /></li>
-                <li><img src="" alt="" /></li>
+            <ul className="d-flex align-center gap-20">
+                <h3 className="text-blu">FOLLOW US</h3>
+                <li><a href=""><img src={Facebook} alt="" /></a></li>
+                <li><a href=""><img src={Twitter} alt="" /></a></li>
+                <li><a href=""><img src={Youtube} alt="" /></a></li>
+                <li><a href=""><img src={Pinterest} alt="" /></a></li>
+                <li><a href=""><img src={Periscope} alt="" /></a></li>
             </ul>
         </nav>
     )

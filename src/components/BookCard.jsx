4a3aa9a2bb5c1f3b3fbd77f53cf6e6d9dfa02e1e
matteolpera <matteo.lepera02@gmail.com>
2025-11-26 16:25:02 +0100

    aggiustato css bookcard

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,20 +1,19 @@
 import { comics } from "../components/comics"
-import "../components/BookCard.css"
+import BookCardStyle from "../components/BookCard.module.css"
 
 export default function BookCard() {
+    const containerClass = `container d-flex justify-center ${BookCardStyle.gap} flex-wrap ${BookCardStyle.content}`
     return (
-        <div className="container d-flex justify-center gap flex-wrap content">
+        <div className={containerClass}>
             {comics.map((comic) =>
-                <div className="col" key={comic.id}>
+                <div className={BookCardStyle.col} key={comic.id}>
                     <img src={comic.thumb} height="200" width="200"
                         alt={comic.title} />
                     <h3>{comic.series.toUpperCase()}</h3>
                 </div>
-
             )
             }
-
-            <a className="btn-load" href="">LOAD MORE</a>
+            <a className={BookCardStyle.a} href="">LOAD MORE</a>
         </div >
     )
 }

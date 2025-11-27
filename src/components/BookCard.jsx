7fd4edb88f98662a5e089cd4813d282d085b8634
matteolpera <matteo.lepera02@gmail.com>
2025-11-27 13:31:07 +0100

    feat: aggiustate la card con props. wip su bottone e badge.

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,22 +1,18 @@
-import { comics } from "../components/comics"
+
 import BookCardStyle from "../components/BookCard.module.css"
 import Badge from "./Badge";
 
-export default function BookCard() {
-    const containerClass = `container d-flex justify-center ${BookCardStyle.gap} flex-wrap ${BookCardStyle.content} position-relative`
+export default function BookCard({ thumb, title, series }) {
     const colClass = `mt-10 ${BookCardStyle.col}`
     return (
-        <div className={containerClass}>
-            <Badge />
-            {comics.map((comic) =>
-                <div className={colClass} key={comic.id}>
-                    <img src={comic.thumb} height="200" width="200"
-                        alt={comic.title} />
-                    <h3>{comic.series.toUpperCase()}</h3>
-                </div>
-            )
-            }
-            <a className={BookCardStyle.a} href="">LOAD MORE</a>
-        </div >
+        <>
+            {/* <Badge /> */}
+            <div className={colClass}>
+                <img src={thumb} height="200" width="200"
+                    alt={title} />
+                <h3>{series}</h3>
+            </div>
+            {/* <a className={BookCardStyle.a} href="">LOAD MORE</a> */}
+        </ >
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,15 +5,27 @@ import ComicsSubscriptions from "../assets/img/buy-comics-subscriptions.png";
 import ComicsShop from "../assets/img/buy-comics-shop-locator.png";
 import DcPowerVisa from "../assets/img/buy-dc-power-visa.svg";
 import BookCard from "./BookCard";
+import { comics } from "../components/comics"
 
 export default function Main() {
     return (
         <main className={stylemain.background}>
             <div className={stylemain.banner}></div>
-            <BookCard />
+            <div className="container d-flex flex-wrap justify-center position-relative content">
+                {comics.map((comic) =>
+                    <BookCard
+                        key={comic.id}
+                        thumb={comic.thumb}
+                        title={comic.title}
+                        series={comic.series.toUpperCase()} />
+                )}
+            </div>
             <div className={stylemain.wrap}>
                 <div className="container">
                     <ul className="d-flex gap-20 justify-between p-30">
+                        <div className="col">
+
+                        </div>
                         <li><img src={ComicsDigital} alt="" />DIGITAL COMICS</li>
                         <li><img src={ComiscsMerc} alt="" />DC MERCHANDAISE</li>
                         <li><img src={ComicsSubscriptions} alt="" />SUBSCRIPTION</li>

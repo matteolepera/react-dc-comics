5087017ab3e262fc043ac335e71d49b960350362
matteolpera <matteo.lepera02@gmail.com>
2025-11-27 15:32:32 +0100

    feat: aggiustato footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,44 +6,56 @@ export default function Footer() {
     return (
         <footer>
             <div className="container footer-top d-flex gap-20">
-                <ul>
+
+                <div className="col">
                     <h3>DC COMICS</h3>
-                    <li><a href="">Characters</a></li>
-                    <li><a href="">Comics</a></li>
-                    <li><a href="">Movies</a></li>
-                    <li><a href="">Tv</a></li>
-                    <li><a href="">Games</a></li>
-                    <li><a href="">Videos</a></li>
-                    <li><a href="">News</a></li>
-                    <ul className="mt-10">
-                        <h3>SHOP</h3>
+                    <ul>
+                        <li><a href="">Characters</a></li>
+                        <li><a href="">Comics</a></li>
+                        <li><a href="">Movies</a></li>
+                        <li><a href="">Tv</a></li>
+                        <li><a href="">Games</a></li>
+                        <li><a href="">Videos</a></li>
+                        <li><a href="">News</a></li>
+                    </ul>
+
+                    <h3>SHOP</h3>
+                    <ul>
                         <li><a href="">Shop DC</a></li>
                         <li><a href="">Shop DC Collectibles</a></li>
                     </ul>
-                </ul>
-                <ul>
+                </div>
+
+                <div className="col">
                     <h3>DC</h3>
-                    <li><a href="">Terms Of Use</a></li>
-                    <li><a href="">Privacy policy (New)</a></li>
-                    <li><a href="">Ad Choices</a></li>
-                    <li><a href="">Advertising</a></li>
-                    <li><a href="">Jobs</a></li>
-                    <li><a href="">Subscriptions</a></li>
-                    <li><a href="">Talent Worshops</a></li>
-                    <li><a href="">CPSC Certificates</a></li>
-                    <li><a href="">Ratings</a></li>
-                    <li><a href="">Shop Help</a></li>
-                    <li><a href="">Contact Us</a></li>
-                </ul>
-                <ul>
+                    <ul>
+                        <li><a href="">Terms Of Use</a></li>
+                        <li><a href="">Privacy policy (New)</a></li>
+                        <li><a href="">Ad Choices</a></li>
+                        <li><a href="">Advertising</a></li>
+                        <li><a href="">Jobs</a></li>
+                        <li><a href="">Subscriptions</a></li>
+                        <li><a href="">Talent Workshops</a></li>
+                        <li><a href="">CPSC Certificates</a></li>
+                        <li><a href="">Ratings</a></li>
+                        <li><a href="">Shop Help</a></li>
+                        <li><a href="">Contact Us</a></li>
+                    </ul>
+                </div>
+
+                <div className="col">
                     <h3>SITES</h3>
-                    <li><a href="">DC</a></li>
-                    <li><a href="">MAD Magazine</a></li>
-                    <li><a href="">DC Kids</a></li>
-                    <li><a href="">DC Universe</a></li>
-                    <li><a href="">DC Power Visa</a></li>
-                </ul>
+                    <ul>
+                        <li><a href="">DC</a></li>
+                        <li><a href="">MAD Magazine</a></li>
+                        <li><a href="">DC Kids</a></li>
+                        <li><a href="">DC Universe</a></li>
+                        <li><a href="">DC Power Visa</a></li>
+                    </ul>
+                </div>
+
             </div>
+
 
             <div className="footer-bottom">
                 <div className="container d-flex justify-between">
